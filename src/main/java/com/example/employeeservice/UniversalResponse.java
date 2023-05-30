abb8d155a39bb3e83f1722b6110d1970f2724ab2
KaraniDev <karani.maina376@gmail.com>
2023-05-30 23:58:07 +0300

    update proects

--- src/main/java/com/example/employeeservice/UniversalResponse.java
+++ src/main/java/com/example/employeeservice/UniversalResponse.java
@@ -3,8 +3,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 @Builder
 
-
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UniversalResponse (int  status, Object data, String message){
   public static UniversalResponseBuilder builder(){
