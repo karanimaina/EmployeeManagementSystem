0f2480ae94341df31a42edb7297efa3ac9a75ddf
KaraniDev <karani.maina376@gmail.com>
2023-05-30 23:47:42 +0300

    update proects

--- src/main/java/com/example/employeeservice/UniversalResponse.java
+++ src/main/java/com/example/employeeservice/UniversalResponse.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 @Builder
 
 
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record UniversalResponse (int  status, Object data, String message){
   public static UniversalResponseBuilder builder(){
